24d0557d2de4e1d9be18bf7d90ea390a22fdf8f7
MiaadEstakhri <miaad8104@gmail.com>
2024-02-27 23:50:38 +0330

    add icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,3 @@
 export * from "./locationIcon";
+export * from "./minusIcon";
+export * from "./plusIcon";
---
+++ src/assets/icons/minusIcon.jsx
@@ -0,0 +1,13 @@
+export function MinusIcon({ stroke = "currentColor", className = "w-6 h-6" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={3}
+      stroke={stroke}
+      className={className}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+    </svg>
+  );
+}
