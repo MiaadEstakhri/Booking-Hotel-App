06f960bacd5659520ec378d18beace5de5d87d0c
MiaadEstakhri <miaad8104@gmail.com>
2024-02-27 21:58:17 +0330

    make header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { LocationIcon } from "../../assets/icons";
+
+export function Header() {
+  return (
+    <nav className="flex justify-center">
+      <div className="h-full w-full container flex items-center justify-between border border-solid border-gray-200 rounded-2xl p-4">
+        <div className=" flex items-center gap-2">
+          <LocationIcon className="w-5 h-5 " fill="#ef4444" />
+          <input
+            type="text"
+            className="w-full outline-none border-0 text-sm"
+            placeholder="Where to go?"
+          />
+        </div>
+        <span className="h-8 border-0 border-r-2 border-solid border-gray-200 "></span>
+        <div className="">b</div>
+        <span className="h-8 border-0 border-r-2 border-solid border-gray-200 "></span>
+
+        <div className="">c</div>
+        <span className="h-8 border-0 border-r-2 border-solid border-gray-200 "></span>
+
+        <div className="">d</div>
+      </div>
+    </nav>
+  );
+}
