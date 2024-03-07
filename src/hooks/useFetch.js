3c573cdeff314253e85ec75ea96bd08278fe8355
MiaadEstakhri <miaad8104@gmail.com>
2024-03-07 19:38:33 +0330

    make useFetch custom Hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
-export default function useFetch({ url = "", query = "" }) {
+export default function useFetch(url, query = "") {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
