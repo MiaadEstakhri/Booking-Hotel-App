44bbd7bafdd0b724ebd69c524462c07abeb159c9
MiaadEstakhri <miaad8104@gmail.com>
2024-03-08 21:07:50 +0330

    make appLayout component

---
+++ src/components/appLayout/appLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function AppLayout() {
+  return (
+    <div className="grid grid-cols-2">
+      <section className="">side</section>
+      <section className="">map</section>
+    </div>
+  );
+}
