bdd041281a8b99f613c49c62057ea30ed8f4ed0a
MiaadEstakhri <miaad8104@gmail.com>
2024-03-08 21:06:49 +0330

    use of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Toaster } from "react-hot-toast";
-import { Header, LocationList } from "./components";
+import { Route, Routes } from "react-router-dom";
+import { AppLayout, Header, LocationList } from "./components";
 import "./App.css";
 
 function App() {
   return (
-    <main className="">
+    <main className="mx-5">
       <Toaster />
       <Header />
-      <LocationList />
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}></Route>
+      </Routes>
     </main>
   );
 }
